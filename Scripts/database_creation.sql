21e71b2d9c23b1ef83b8190e33ad821ec8d7dab4
Emmanuel Obayomi <eobayomi2@gmail.com>
2025-08-31 01:56:11 +0100

    Added script to create a database called sme_credit and a schema called sme

---
+++ Scripts/database_creation.sql
@@ -0,0 +1,26 @@
+/*
+=======================================================================================
+CREATE OR REPLACE DATABASE AND SCHEMA 
+=======================================================================================
+The following script creates a database called "sme_credit."
+It first checks if the database already exists; if it does, the script drops 
+the existing database and creates a new one. After that, it creates schema called sme.
+
+WARNING:
+	If you run this script, it will drop the sme_database Database and its data content
+	if it happens to exist. Please ensure you have backed up the database before running this script. 
+	Please visit:https://learn.microsoft.com/en-us/sql/relational-databases/backup-restore/create-a-full-database-backup-sql-server?view=sql-server-ver16
+	for more information about backing up a database.
+*/
+
+
+-- USE MASTER USER 
+USE MASTER;
+GO;
+-- check if sme_credit db exist. drop if it does
+IF DB_ID('sme_credit') IS NOT NULL DROP DATABASE sme_credit;
+CREATE DATABASE sme_credit;
+GO;
+USE sme_credit;
+GO;
+CREATE SCHEMA sme
